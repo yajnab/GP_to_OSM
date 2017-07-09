fa2a451d806b274e03f2dda10d47cc880c41f37c
Yajnavalkya Bandyopadhyay <yajnab@gmail.com>
2017-07-09 18:41:51 +0530

    osmgen.java : Introducing a new class for creation of OSM XML lines
    Main.java parses the latitude and longitude values which are exported here for creation of the xml lines in accordance to the OSM export file format.

---
+++ src/java/gp_to_osm/osmgen.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gp_to_osm;
+
+import javax.json.JsonNumber;
+
+/**
+ *
+ * @author Pooja Nigam
+ */
+public class osmgen {
+    public void osmgenerator(JsonNumber lat, JsonNumber lng){
+        
+         randn randnum = (new randn()); //Call randum number generating class
+         
+         String node = "<node id=\""+ randnum.numbera() + "\" visible=\"true\" version=\"1\" changeset=\""+ randnum.numbera() + "\" timestamp=\"2009-11-18T22:51:06Z\" user=\"yajnab\" uid=\"111111\" lat=\"" + lat +  "\" lon=\""+lng+"\">\n" +
+"  <tag k=\"hospital\" v=\"hospital\"/>\n" +
+" </node>";
+         System.out.println(node);      
+    }
+    
+}
