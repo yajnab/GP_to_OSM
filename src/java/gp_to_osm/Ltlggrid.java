05703f88d18fb09b93e298ee0ac010ac4d582262
Yajnavalkya Bandyopadhyay <yajnab@gmail.com>
2017-07-17 23:07:20 +0530

    Ltlggrid : Update Values for Latitutes and Longitudes

--- src/java/gp_to_osm/Ltlggrid.java
+++ src/java/gp_to_osm/Ltlggrid.java
@@ -15,13 +15,13 @@ public class Ltlggrid {
      * @param args the command line arguments
      */
     public static void d_gen() {
-        double x1 = 26.918360, x2 = 26.913923;
-        double y1 = 75.786008, y2= 75.779632;
+        double x1 = 26.8681, x2 = 26.8405;
+        double y1 = 75.8003, y2= 75.8254;
         double xs = (x2-x1)/150;
         double ys = (y2-y1)/150;
         for(int i=0;i<150;i++){
             for(int j=0;j<150;j++){
-                System.out.println((x1+i*xs)+" "+(y1+j*ys));
+                System.out.println((x1+i*xs)+","+(y1+j*ys));
             }
         }
     }
