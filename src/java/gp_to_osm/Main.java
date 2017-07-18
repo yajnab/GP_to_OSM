8dec54a5f09c63bd4f33646ebf5cfb8287e622d6
Yajnavalkya Bandyopadhyay <yajnab@gmail.com>
2017-07-18 19:17:31 +0530

    jsonpar.java : Moved JSONParsing function here

--- src/java/gp_to_osm/Main.java
+++ src/java/gp_to_osm/Main.java
@@ -23,7 +23,7 @@ public class Main {
      */
     public static void main(String[] args) throws Exception{
         int c = 0;
-        Main m = new Main();
+        jsonpar m = new jsonpar();
         jsongen jg = new jsongen();
         Ltlggrid ltd = new Ltlggrid();
         System.out.println("1 for download, 2 for processing, 3 for Coordinate Generation");
@@ -43,38 +43,7 @@ public class Main {
                 jg.download();
                 break;
         } 
-   }
-    public void process(){
-        try{
-          String[] file = {"hospital", "restaurants", "museum", "bank", "atm", "movie", "mosque", 
-              "hindu_temple", "church", "school", "university", "store", "gym", "doctor"};
-          for(int it=0;it<file.length;it++){
-          String JSON_FILE=file[it]+".txt";
-          InputStream fis = new FileInputStream(JSON_FILE);		
-	//create JsonReader object
-	JsonReader reader = Json.createReader(fis);
-        JsonObject jobj = reader.readObject();
-         //JsonObject jobjS = jobj.getJsonObject("results");
-         JsonArray res = (JsonArray) jobj.get("results");
-         
-       try{
-         for(int i=0; i< res.size();i++){
-                 JsonObject geo = res.getJsonObject(i);
-                 JsonObject geoloc = geo.getJsonObject("geometry").getJsonObject("location");
-                 JsonNumber lat = geoloc.getJsonNumber("lat");
-                 JsonNumber lng = geoloc.getJsonNumber("lng");
-                 osmgen og = new osmgen();
-                 og.osmgenerator(lat, lng, file[it]);
-             }
-         }
-         catch(Exception e){
-             System.err.println(e);
-         }
-      }}
-      catch(FileNotFoundException ex){
-          System.err.println(ex);
-      }
-    }
+   } 
     
 }
     
---
+++ src/java/gp_to_osm/jsonpar.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gp_to_osm;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonNumber;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+
+/**
+ *
+ * @author Pooja Nigam
+ */
+public class jsonpar {
+    public void process(){
+        try{
+          String[] file = {"hospital", "restaurants", "museum", "bank", "atm", "movie", "mosque", 
+              "hindu_temple", "church", "school", "university", "store", "gym", "doctor"};
+          for(int it=0;it<file.length;it++){
+          String JSON_FILE=file[it]+".txt";
+          InputStream fis = new FileInputStream(JSON_FILE);		
+	//create JsonReader object
+	JsonReader reader = Json.createReader(fis);
+        JsonObject jobj = reader.readObject();
+         //JsonObject jobjS = jobj.getJsonObject("results");
+         JsonArray res = (JsonArray) jobj.get("results");
+         
+       try{
+         for(int i=0; i< res.size();i++){
+                 JsonObject geo = res.getJsonObject(i);
+                 JsonObject geoloc = geo.getJsonObject("geometry").getJsonObject("location");
+                 JsonNumber lat = geoloc.getJsonNumber("lat");
+                 JsonNumber lng = geoloc.getJsonNumber("lng");
+                 osmgen og = new osmgen();
+                 og.osmgenerator(lat, lng, file[it]);
+             }
+         }
+         catch(Exception e){
+             System.err.println(e);
+         }
+      }}
+      catch(FileNotFoundException ex){
+          System.err.println(ex);
+      }
+    }
+}
