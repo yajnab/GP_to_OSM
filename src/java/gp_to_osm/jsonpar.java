933bd1d6bc96aeb59cc4f648d1e16c46c776546d
Yajnavalkya Bandyopadhyay <yajnab@gmail.com>
2017-07-18 20:02:42 +0530

    jsonpar.java : Files are segmented into multiple files with numbered files

--- src/java/gp_to_osm/jsonpar.java
+++ src/java/gp_to_osm/jsonpar.java
@@ -21,33 +21,38 @@ import javax.json.JsonReader;
 public class jsonpar {
     public void process(){
         try{
-          String[] file = {"hospital", "restaurants", "museum", "bank", "atm", "movie", "mosque", 
+          String[] file = {"hospital", "restaurant", "museum", "bank", "atm", "movie", "mosque", 
               "hindu_temple", "church", "school", "university", "store", "gym", "doctor"};
           for(int it=0;it<file.length;it++){
-          String JSON_FILE=file[it]+".txt";
-          InputStream fis = new FileInputStream(JSON_FILE);		
-	//create JsonReader object
-	JsonReader reader = Json.createReader(fis);
-        JsonObject jobj = reader.readObject();
-         //JsonObject jobjS = jobj.getJsonObject("results");
-         JsonArray res = (JsonArray) jobj.get("results");
+              for(int j=1;j<=4;j++ ){
+                try{
+                    String JSON_FILE="data/"+file[it]+j+".json";
+                    InputStream fis = new FileInputStream(JSON_FILE);		
+                    //create JsonReader object
+                    JsonReader reader = Json.createReader(fis);
+                    JsonObject jobj = reader.readObject();
+                    //JsonObject jobjS = jobj.getJsonObject("results");
+                    JsonArray res = (JsonArray) jobj.get("results");
          
-       try{
-         for(int i=0; i< res.size();i++){
-                 JsonObject geo = res.getJsonObject(i);
-                 JsonObject geoloc = geo.getJsonObject("geometry").getJsonObject("location");
-                 JsonNumber lat = geoloc.getJsonNumber("lat");
-                 JsonNumber lng = geoloc.getJsonNumber("lng");
-                 osmgen og = new osmgen();
-                 og.osmgenerator(lat, lng, file[it]);
-             }
-         }
-         catch(Exception e){
-             System.err.println(e);
-         }
-      }}
-      catch(FileNotFoundException ex){
-          System.err.println(ex);
-      }
+                        try{
+                            for(int i=0; i< res.size();i++){
+                                JsonObject geo = res.getJsonObject(i);
+                                JsonObject geoloc = geo.getJsonObject("geometry").getJsonObject("location");
+                                JsonNumber lat = geoloc.getJsonNumber("lat");
+                                JsonNumber lng = geoloc.getJsonNumber("lng");
+                                osmgen og = new osmgen();
+                                og.osmgenerator(lat, lng, file[it]);
+                            }
+                        }
+                        catch(Exception e){
+                            System.err.println(e);
+                        }
+                }
+                catch(Exception e){}
+              } 
+          }
+        }
+        catch(Exception e){}
+       
     }
 }
